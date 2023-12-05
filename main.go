5f376a6190a7601846c621175834e5c3cfcb9fc1
Tyler <khuuthanh1001@gmail.com>
2023-12-05 13:38:41 +0700

    loggin the messages

--- main.go
+++ main.go
@@ -2,34 +2,74 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"os"
+	"sort"
 	"strconv"
+	"sync"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
+type Message struct {
+	Payload          interface{} `json:"payload"`
+	CreatedAt        string      `json:"created_at`
+	CreatedTimestamp int64       `json:"-"`
+}
+
+type Messages struct {
+	mu   sync.Mutex
+	Msgs []Message
+}
+
 func main() {
 	router := gin.New()
+	router.LoadHTMLFiles("./index.html")
+
+	msgs := Messages{}
+	maxMsg := 30
 
 	router.Use(
 		gin.Logger(),
 	)
 
 	router.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"status": "OK",
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"messages": msgs.Msgs,
 		})
 	})
 
 	router.POST("/", func(c *gin.Context) {
-		data, err := ioutil.ReadAll(c.Request.Body)
+		now := time.Now()
+		data, err := io.ReadAll(c.Request.Body)
 		if err != nil {
 			fmt.Println("err", err.Error())
-		} else {
-			fmt.Printf("\n\nrequest payload: %v\n", string(data))
+			c.JSON(http.StatusOK, gin.H{})
+
+			return
 		}
+
+		msg := Message{
+			Payload:          string(data),
+			CreatedAt:        now.Format(time.ANSIC),
+			CreatedTimestamp: now.Unix(),
+		}
+
+		msgs.mu.Lock()
+		msgs.Msgs = append(msgs.Msgs, msg)
+
+		sort.Slice(msgs.Msgs, func(i, j int) bool {
+			return msgs.Msgs[i].CreatedTimestamp > msgs.Msgs[j].CreatedTimestamp
+		})
+
+		// remove old msgs
+		if len(msgs.Msgs) > maxMsg {
+			msgs.Msgs = msgs.Msgs[:maxMsg]
+		}
+
+		msgs.mu.Unlock()
 
 		c.JSON(http.StatusOK, gin.H{})
 	})
